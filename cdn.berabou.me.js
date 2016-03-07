444a4c5bd5d349151035d1dbb088cf2dfe42ba72
59naga <i59naga@icloud.com>
2016-03-07 12:36:19 +0900

    fix: `Notfound package: bundle.min.js`

--- cdn.berabou.me.js
+++ cdn.berabou.me.js
@@ -9,12 +9,12 @@ const app = express();
 app.disable('x-powered-by');
 app.use(cors());
 app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(npmcdn(path.join(__dirname, 'public', 'packages'), {
   api: 'http://registry.npmjs.org',
   maxAge: 60 * 60 * 24 * 365, // one year
   extensions: ['', '.js', '.json', '.html'],
 }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   process.stdout.write(`npmcdn is available on http://localhost:${port}\n`);
 });
